829fb915e2b1e921295be91c16f7fedff10362a4
Ilya Kolpakov <ikolpakov@health2030.ch>
2023-04-26 10:30:28 +0200

    add jupyter-notebook as default app in the flake

--- flake.nix
+++ flake.nix
@@ -3,9 +3,14 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
   };
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.default = 
-      let pkgs = import nixpkgs { system = "x86_64-linux"; };
-      in import ./python3.nix { inherit pkgs; };
-  };
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+      python3 = import ./python3.nix { inherit pkgs; };
+    in {
+      #packages.x86_64-linux.default = python3;
+      apps.x86_64-linux.default = { type = "app"; program = "${python3}/bin/jupyter-notebook"; };
+      apps.x86_64-linux.python3 = { type = "app"; program = "${python3}/bin/python3"; };
+      apps.x86_64-linux.ipython = { type = "app"; program = "${python3}/bin/ipython"; };
+    };
 }
