9fe09245a3ed809f0d51825f441a68a2fa8d83b9
Ilya Kolpakov <ikolpakov@health2030.ch>
2024-02-21 19:17:33 +0100

    switch nixpkgs back to 23.11

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "A Jupyter notebook environment for single cell analysis";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
   };
   outputs = { self, nixpkgs }:
     let
